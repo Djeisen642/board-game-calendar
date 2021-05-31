b45ed6e099d0554eda84092629a2a03ddb7d4331
Jason Suttles <jasonssuttles@gmail.com>
2021-05-31 13:00:21 -0400

    feat: ts eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:jsdoc/recommended'
   ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
   plugins: [
     '@typescript-eslint',
     'jsdoc'
