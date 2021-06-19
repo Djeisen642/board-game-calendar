ad5277057ff81f0c5829583a283fecdfc61bd078
Jason Suttles <jasonssuttles@gmail.com>
2021-06-18 22:33:09 -0400

    fix: try commenting out auth routing

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,13 @@
 import { Context } from '@nuxt/types'
-import GameCollection from '~/pages/GameCollection.vue'
-import SignIn from '~/pages/SignIn.vue'
-import index from '~/pages/index.vue'
+// import GameCollection from '~/pages/GameCollection.vue'
+// import SignIn from '~/pages/SignIn.vue'
+// import index from '~/pages/index.vue'
 
 export default function ({ store, redirect, route }:Context):void {
-  if (store.state.user) {
-    route.name === SignIn.routeName && redirect(GameCollection.route)
-  } else if (route.name) {
-    ![index.routeName, SignIn.routeName].includes(route.name) && redirect(SignIn.route)
-  }
+  console.log(route.name)
+  // if (store.state.user) {
+  //   route.name === SignIn.routeName && redirect(GameCollection.route)
+  // } else if (route.name) {
+  //   ![index.routeName, SignIn.routeName].includes(route.name) && redirect(SignIn.route)
+  // }
 }
