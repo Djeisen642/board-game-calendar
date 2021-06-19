06111832b41433a242ee37527d48058b16d707c3
Jason Suttles <jasonssuttles@gmail.com>
2021-06-18 23:02:55 -0400

    feat: setup the index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,70 +3,30 @@
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
         <logo />
-        <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Board Game Calendar!
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
+            This calendar is currently a work in progress.
+            The goal of this calendar is to facilitate people coming together
+            to play specific board games instead of the game that attracts the least
+            common denominator.
           </p>
           <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
+            To do this, users will be able to add their games to their collection;
+            they will be able to add friends; they will be able to invite these friends
+            to board game sessions to play one or more board games.
           </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/inspire"
+            to="/SignIn"
           >
             Continue
           </v-btn>
@@ -79,11 +39,10 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import { NuxtHeadType } from '~/constants/types'
 
 @Component({
-  components: { Logo, VuetifyLogo }
+  components: { Logo }
 })
 export default class index extends Vue {
   static route = '/'
