f627be42e8a8ee068aca778443540e94af2c5b24
Jason Suttles <jasonssuttles@gmail.com>
2021-06-18 22:46:44 -0400

    fix: uncomment the auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,13 +1,12 @@
 import { Context } from '@nuxt/types'
-// import GameCollection from '~/pages/GameCollection.vue'
-// import SignIn from '~/pages/SignIn.vue'
-// import index from '~/pages/index.vue'
+import SignIn from '~/pages/SignIn.vue'
+import index from '~/pages/index.vue'
+import GameCollection from '~/pages/GameCollection.vue'
 
 export default function ({ store, redirect, route }:Context):void {
-  console.log(route.name)
-  // if (store.state.user) {
-  //   route.name === SignIn.routeName && redirect(GameCollection.route)
-  // } else if (route.name) {
-  //   ![index.routeName, SignIn.routeName].includes(route.name) && redirect(SignIn.route)
-  // }
+  if (store.state.user) {
+    route.name === SignIn.routeName && redirect(GameCollection.route)
+  } else if (route.name) {
+    ![index.routeName, SignIn.routeName].includes(route.name) && redirect(SignIn.route)
+  }
 }
