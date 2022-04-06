245caa7b5856b8a944b1a5298371ba4c02cdae5b
JasonS <jasonssuttles@gmail.com>
2022-04-05 22:45:45 -0400

    fix: remove some ids and keys

--- firebase.config.js
+++ firebase.config.js
@@ -1,24 +1,24 @@
 module.exports =
   process.env.NODE_ENV === 'development'
     ? {
-        apiKey: 'AIzaSyB7YMCuegHZ9U4wxAbUQIQnu-qQ8redrZk',
+        apiKey: process.env.G_API_KEY,
         authDomain: 'board-game-calendar-local.firebaseapp.com',
         databaseURL:
           'https://board-game-calendar-local-default-rtdb.firebaseio.com',
         projectId: 'board-game-calendar-local',
         storageBucket: 'board-game-calendar-local.appspot.com',
         messagingSenderId: '342985459417',
-        appId: '1:342985459417:web:bac2854c7d338b430de0c8',
-        measurementId: 'G-BWNCEJVFBH',
+        appId: process.env.G_APP_ID,
+        measurementId: 'G-NHS710XDXN',
       }
     : {
-        apiKey: 'AIzaSyDybqGTKLaLM4kzX6jkma68EjapWItEgYI',
+        apiKey: process.env.G_API_KEY,
         authDomain: 'board-game-calendar-315218.firebaseapp.com',
         databaseURL:
           'https://board-game-calendar-315218-default-rtdb.firebaseio.com',
         projectId: 'board-game-calendar-315218',
         storageBucket: 'board-game-calendar-315218.appspot.com',
         messagingSenderId: '330277334689',
-        appId: '1:330277334689:web:a2e7bc335e3c36239f4ae0',
+        appId: process.env.G_APP_ID,
         measurementId: 'G-PD6PE4H25Y',
       }
