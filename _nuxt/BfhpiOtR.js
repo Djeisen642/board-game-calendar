a9229bb599189711a70b9ff444d2db36767fba62
Djeisen642 <Djeisen642@users.noreply.github.com>
2026-06-07 17:00:06 +0000

    deploy: 9fbbb01ce34749b424138c5684d13eda8d0e77c0

---
+++ _nuxt/BfhpiOtR.js
@@ -0,0 +1,1 @@
+const e={BoardGameGeekBaseUrl:"/api/bgg/",BggBoardGameType:"boardgame",DebounceThrottleInMs:500,NumberToShow:10,PrimaryNameType:"primary",LoadingTimeoutInMs:2e3};export{e as c};
--- _nuxt/DCdazUUK.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./Cw743U8Q.js";const e=a("v-spacer","div","VSpacer");export{e as V};
